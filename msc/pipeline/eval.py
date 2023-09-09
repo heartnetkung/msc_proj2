8a36df91532225dc7f989c7f2eb12fccb8df0964
heartnetkung <heartnetkung@gmail.com>
2023-09-09 15:58:40 +0100

    add precision_all

--- msc/pipeline/eval.py
+++ msc/pipeline/eval.py
@@ -18,6 +18,31 @@ def precision_at_r(df, cdist, r):
             'avg_neighbor': denominator/len(df)}
 
 
+def precision_all(df, cdist, max_radius=None):
+    if max_radius is None:
+        max_radius = np.max(cdist)
+    numerator = np.zeros(max_radius+1, dtype=int)
+    denominator = np.zeros(max_radius+1, dtype=int)
+    epitopes = df['epitope'].tolist()
+
+    for i in range(len(cdist)):
+        center_epitope, row = epitopes[i], cdist[i]
+        for j in range(len(cdist)):
+            dist = row[j]
+            if i == j or dist > max_radius:
+                continue
+
+            denominator[dist] += 1
+            if center_epitope == epitopes[j]:
+                numerator[dist] += 1
+
+    numerator = np.cumsum(numerator)
+    denominator = np.cumsum(denominator)
+    return pd.DataFrame({'precision': numerator/denominator,
+                         'avg_neighbor': denominator/len(df),
+                         'radius': range(max_radius+1)})
+
+
 def eval_meysman(df, cluster_col='cluster'):
     def mode(x): return stats.mode(x)[1][0]
     len_all = len(df)
