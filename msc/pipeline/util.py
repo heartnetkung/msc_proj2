8a611972792e04fd0ad227e15a9a81fc1fc682f6
heartnetkung <heartnetkung@gmail.com>
2023-09-09 23:24:49 +0100

    implement mining procedure

--- msc/pipeline/util.py
+++ msc/pipeline/util.py
@@ -52,24 +52,48 @@ def read_emerson(single_file=False):
     return ans.rename(columns=fieldmap)[fieldmap.values()]
 
 
+def read_template():
+    _file = path.abspath(path.join(__file__, '../../../data/templategt8.csv'))
+    return pd.read_csv(_file, index_col=False)
+
+
+def print_count(arr):
+    u, c = np.unique(arr, return_counts=True)
+    for i in range(len(u)):
+        print(u[i], c[i])
+
+
 def read_emerson_healthy(single_file=False):
-    folder = path.abspath(path.join(__file__, '../../../data/emerson_healthy'))
+    folder = path.abspath(path.join(__file__, '../../../../msc_proj/data/emerson'))
     ans = None
-    for i, file in enumerate(listdir(folder)):
+    files = listdir(folder)
+    total_tcr = 0
+    for i, file in enumerate(files):
+        print(i/len(files))
         df = pd.read_csv(path.abspath(path.join(folder, file)),
-                         sep='\t', usecols=['amino_acid', 'v_gene'])
+                         sep='\t', usecols=['amino_acid','templates'])
+        df['templates'].fillna(1,inplace=True)
         df.dropna(inplace=True)
+        total_tcr += len(df)
+        if len(df)==0:
+            continue
         df = df[df['amino_acid'].str.match(
-            r'^C[ACDEFGHIKLMNPQRSTVWY]{9,16}[FW]$')]
-        df = df[df['v_gene'].str.contains(r'\-\d\d$')]
+            r'^C[ACDEFGHIKLMNPQRSTVWY]{9,16}[FW]$')].reset_index(drop=True)
+
+        df = df.groupby('amino_acid')['templates'].sum().reset_index()
         df['file'] = i
+        filter1 = df['templates'] > 6
+        filter2 = np.all((df['amino_acid'].str.len()==14,df['templates'] > 4),axis=0)
+        filter3 = np.all((df['amino_acid'].str.len()>14,df['templates'] > 1),axis=0)
+        df = df[np.logical_or(filter1,filter2)]
         if ans is None:
-            ans = df[['amino_acid', 'file', 'v_gene']].reset_index(drop=True)
+            ans = df[['amino_acid', 'file']].reset_index(drop=True)
         else:
             ans = pd.concat([ans, df], copy=False, ignore_index=True)
         if single_file:
             break
-    fieldmap = {'v_gene': 'vb', 'amino_acid': 'cdr3b', 'file': 'file'}
+    print('total_tcr:',total_tcr)
+    fieldmap = {'amino_acid': 'cdr3b', 'file': 'file'}
     return ans.rename(columns=fieldmap)[fieldmap.values()]
 
 
